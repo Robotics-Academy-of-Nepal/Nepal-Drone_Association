bd93b2ec31969a8a669c24bf6a37ed758637a813
BibashDahal1 <bibash98425@gmail.com>
2025-02-02 15:36:51 +0545

    changes

--- src/About.jsx
+++ src/About.jsx
@@ -10,7 +10,7 @@ function About(){
         <Navbar />
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-2">
       <div className="p-1">
-        <div className="relative">
+        <div>
           <img 
             src={drone10}
             alt="Placeholder" 
