35dd6601902a9e2beeaed7f1dbdb41ef9286a015
BibashDahal1 <bibash98425@gmail.com>
2025-02-22 16:08:23 +0545

    commitment

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -61,7 +61,7 @@ const Navbar = ({ onAboutClick, onTeamClick, onNewsClick }) => {
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center space-x-8">
             <Link 
-              to="#" 
+              to="#/" 
               onClick={(e) => handleNavigation(e, onAboutClick, 'about')} 
               className="text-black hover:text-gray-600"
             >
@@ -69,14 +69,14 @@ const Navbar = ({ onAboutClick, onTeamClick, onNewsClick }) => {
             </Link>
             <Link to="/gallery" className="text-black hover:text-gray-600">Gallery</Link>
             <Link 
-              to="#" 
+              to="#/" 
               onClick={(e) => handleNavigation(e, onTeamClick, 'team')} 
               className="text-black hover:text-gray-600"
             >
               Our Team
             </Link>
             <Link 
-              to="#" 
+              to="#/" 
               onClick={(e) => handleNavigation(e, onNewsClick, 'news')} 
               className="text-black hover:text-gray-600"
             >
@@ -108,7 +108,7 @@ const Navbar = ({ onAboutClick, onTeamClick, onNewsClick }) => {
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               <Link
-                to="#" 
+                to="/#/" 
                 onClick={(e) => handleNavigation(e, onAboutClick, 'about')} 
                 className="block px-3 py-2 text-black hover:text-gray-600 hover:bg-gray-50 rounded-md"
               >
@@ -121,14 +121,14 @@ const Navbar = ({ onAboutClick, onTeamClick, onNewsClick }) => {
                 Gallery
               </Link>
               <Link
-                to="/" 
+                to="/#." 
                 onClick={(e) => handleNavigation(e, onTeamClick, 'team')} 
                 className="block px-3 py-2 text-black hover:text-gray-600 hover:bg-gray-50 rounded-md"
               >
                 Our Team
               </Link>
               <Link
-                to="/" 
+                to="/#/" 
                 onClick={(e) => handleNavigation(e, onNewsClick, 'news')} 
                 className="block px-3 py-2 text-black hover:text-gray-600 hover:bg-gray-50 rounded-md"
               >
